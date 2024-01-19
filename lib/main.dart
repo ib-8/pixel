691825c2a80c69a4852e3b4ab51b6b791856be3a
Rajitha <pasala.rajithalakshmi@superops.ai>
2024-01-19 21:17:53 +0530

    dashboard changes

--- lib/main.dart
+++ lib/main.dart
@@ -151,7 +151,7 @@ class _HomeState extends State<Home> {
                   ),
                   HomeCard(
                     lable: 'Expenses',
-                    onTap: () => onTabChange(2),
+                    onTap: () => onTabChange(4),
                   ),
                 ],
               ),
